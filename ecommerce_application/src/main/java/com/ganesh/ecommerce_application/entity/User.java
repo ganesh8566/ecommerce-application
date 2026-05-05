cff462a160a0bb9834add227051cf477aa3c9137
nagam <nagam@Ganesh_Lenovo>
2026-05-05 19:40:25 +0530

    Created POJO for User in entity

---
+++ ecommerce_application/src/main/java/com/ganesh/ecommerce_application/entity/User.java
@@ -0,0 +1,66 @@
+package com.ganesh.ecommerce_application.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity   // This class will become a table in DB
+@Table(name = "users")   // Table name in MySQL
+public class User {
+
+    @Id   // Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto increment
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    // Default constructor (required by JPA)
+    public User() {}
+
+    // Constructor for easy object creation
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters & Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
